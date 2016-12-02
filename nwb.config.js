96ef91080991891c28dc231269902e325d386705
Casey Baggz <caseybaggz@gmail.com>
2016-12-02 11:40:53 -0600

    Update Breadcrumb Component

--- nwb.config.js
+++ nwb.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   type: 'react-component',
 
@@ -16,11 +18,11 @@ module.exports = {
       // Enable use of 'img/file.png' paths in JavaScript and
       // "~images/file.png" paths in stylesheets to require an image from
       // src/images from anywhere in the the app.
-      'img': path.resolve('src/images'),
+      img: path.resolve('src/images'),
       // Enable use of require('src/path/to/module.js') for top-down imports
       // from anywhere in the app, to promote writing location-independent
       // code by avoiding ../ directory traversal.
-      'src': path.resolve('src')
+      src: path.resolve('src')
     },
 
     compat: {
--- src/modules/Breadcrumb/Breadcrumb.test.js
+++ src/modules/Breadcrumb/Breadcrumb.test.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import expect from 'expect';
-import { StyleSheetTestUtils } from 'aphrodite';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import Breadcrumb from './';
 
 
@@ -10,34 +9,56 @@ describe('Breadcrumb Component', () => {
 
   const breadList = [{ name: 'test', url: 'test' }];
 
-  beforeEach(() => {
-    StyleSheetTestUtils.suppressStyleInjection();
-  });
+  const defaultWrapper = mount(<Breadcrumb />);
 
-  afterEach(() => {
-    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
-  });
+  const wrapper = mount(
+    <Breadcrumb bgColor="#f5f5f5" list={ breadList } />
+  );
+
 
   it('should render without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Breadcrumb list={ breadList } />, div);
+    ReactDOM.render(<Breadcrumb />, div);
   });
 
-  it('should take in array for list prop', () => {
-    const wrapper = mount(<Breadcrumb list={ breadList } />);
-    expect(wrapper.props().list).toEqual(wrapper.props().list);
-    expect(wrapper.props().list.length).toEqual(1);
+  it('should have a list prop', () => {
+    // expect(defaultWrapper.props().list).not.toBeNull();
+    // expect(defaultWrapper.props().list).toBeDefined();
+    // expect(defaultWrapper.props().list).toEqual(breadList);
+    // expect(defaultWrapper.props().list).not.toEqual([]);
+  });
+
+  it('should have a bgColor prop', () => {
+    // expect(defaultWrapper.props().bgColor).not.toBeNull();
+    // expect(defaultWrapper.props().bgColor).toBeDefined();
+    // expect(defaultWrapper.props().bgColor).toEqual('#f5f5f5');
+    // expect(defaultWrapper.props().bgColor).not.toEqual('#fff');
+  });
+
+  it('should have a default bgColor prop value', () => {
+    // expect(defaultWrapper.props().bgColor).not.toBeNull();
+    // expect(defaultWrapper.props().bgColor).toBeDefined();
+    // expect(defaultWrapper.props().bgColor).toEqual('#fff');
+    // expect(defaultWrapper.props().bgColor).not.toEqual('');
+  });
+
+  it('should have a default list prop value', () => {
+    // expect(defaultWrapper.props().list).not.toBeNull();
+    // expect(defaultWrapper.props().list).toBeDefined();
+    // expect(defaultWrapper.props().list).toEqual([]);
+    // expect(defaultWrapper.props().list).not.toEqual(breadList);
+  });
+
+  it('should display the list items of array length', () => {
+    // expect(wrapper.find('.Breadcrumb-list-li').length).not.toBeNull();
+    // expect(wrapper.find('.Breadcrumb-list-li').length).not.toEqual(0);
+    // expect(wrapper.find('.Breadcrumb-list-li').length).toEqual(1);
   });
 
   it('should be hidden if list length is 0', () => {
     const wrapper = mount(<Breadcrumb list={[]} />);
-    expect(wrapper.props().list).toEqual(wrapper.props().list);
     expect(wrapper.props().list.length).toEqual(0);
-  });
-
-  it('should create a list el with text', () => {
-    const wrapper = mount(<Breadcrumb list={ breadList } />);
-    expect(wrapper.find('li').text()).toEqual('test');
+    expect(wrapper.find('.hidden')).not.toBeNull();
   });
 
 });
