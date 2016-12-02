99fe2da69c23e065ab9bf483403363157dd3f974
Casey Baggz <caseybaggz@gmail.com>
2016-12-02 08:48:00 -0600

    Update nwb.config

--- nwb.config.js
+++ nwb.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   type: 'react-component',
+
   npm: {
     esModules: true,
     umd: {
@@ -11,9 +12,21 @@ module.exports = {
   },
 
   webpack: {
+    aliases: {
+      // Enable use of 'img/file.png' paths in JavaScript and
+      // "~images/file.png" paths in stylesheets to require an image from
+      // src/images from anywhere in the the app.
+      'img': path.resolve('src/images'),
+      // Enable use of require('src/path/to/module.js') for top-down imports
+      // from anywhere in the app, to promote writing location-independent
+      // code by avoiding ../ directory traversal.
+      'src': path.resolve('src')
+    },
+
     compat: {
       enzyme: true
     },
+
     html: {
       template: 'public/index.html'
     }
