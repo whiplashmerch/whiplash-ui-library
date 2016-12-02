4c2942b75da399a8aff83bf3934833c9ea9b9984
Casey Baggz <caseybaggz@gmail.com>
2016-12-02 08:24:51 -0600

    Update all packages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 export { default as Breadcrumb } from './modules/Breadcrumb';
 export { default as Button } from './modules/Button';
-export { default as CheckButton } from './modules/CheckButton';
 export { default as Input } from './modules/Input';
 export { default as Legend } from './modules/Legend';
 export { default as Modal } from './modules/Modal';
