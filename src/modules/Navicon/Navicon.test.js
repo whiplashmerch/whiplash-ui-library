070e6ec9959b0079eb0dcc365b0a6ca51c0fdfb8
Casey Baggz <caseybaggz@gmail.com>
2016-12-05 16:37:35 -0600

    Update Navicon Component

--- src/modules/Navicon/Navicon.test.js
+++ src/modules/Navicon/Navicon.test.js
@@ -1,51 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import expect from 'expect';
-import { StyleSheetTestUtils } from 'aphrodite';
-import { mount, shallow } from 'enzyme';
+import { expect } from 'chai';
+import { mount } from 'enzyme';
 import Navicon from './';
 
 describe('Navicon Component', () => {
 
-  beforeEach(() => {
-    StyleSheetTestUtils.suppressStyleInjection();
-  });
+  const defaultWrapper = mount(<Navicon />);
 
-  afterEach(() => {
-    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
-  });
+  const wrapper = mount(
+    <Navicon
+      barColor="blue"
+      onUserInput={() => console.log('onUserInput')}
+    />
+  );
+
 
   it('should render without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Navicon />, div);
   });
 
-
-  it('should have a link trigger', () => {
-    const wrapper = shallow(<Navicon />);
-    expect(wrapper.find('.Navicon-link').every('.Navicon-link')).toEqual(true);
-  });
-
-
-  it('should have 3 "bar" elements', () => {
-    const wrapper = shallow(<Navicon />);
-    expect(wrapper.find('.Navicon-bar').length).toEqual(0);
-  });
-
-
-  it('should toggle state when clicked', () => {
-    const func = (bool) => { return; };
-    const wrapper = mount(<Navicon active={ false } onUserInput={ func }/>);
-
-    // active toggle
-    wrapper.find('a').simulate('click');
-    expect(wrapper.props().active).toEqual(false);
-    expect(wrapper.state().finish).toEqual(false);
-
-    // deactive toggle
-    wrapper.find('a').simulate('click');
-    expect(wrapper.props().active).toEqual(false);
-    expect(wrapper.state().finish).toEqual(false);
-  });
 
 });
