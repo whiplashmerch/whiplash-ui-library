f0bbd592d3c8943d32f59bea12f18eb4e86069ad
Casey Baggz <caseybaggz@gmail.com>
2016-12-05 09:27:20 -0600

    Update Button component

--- src/modules/Button/Button.test.js
+++ src/modules/Button/Button.test.js
@@ -1,48 +1,78 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import expect from 'expect';
-import { StyleSheetTestUtils } from 'aphrodite';
-import { mount, shallow } from 'enzyme';
+import { expect } from 'chai';
+import { mount } from 'enzyme';
 import Button from './';
 
 
-describe('Button Component', () => {
+describe('<Button />', () => {
 
-  beforeEach(() => {
-    StyleSheetTestUtils.suppressStyleInjection();
-  });
+  const defaultWrapper = mount(<Button />);
 
-  afterEach(() => {
-    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
-  });
+  const wrapper = mount(
+    <Button
+      buttonText="test button"
+      buttonType="submit"
+      theme="secondary"
+      callback={e => e.preventDefault()}
+    />
+  );
+
 
   it('should render without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Button buttonText="test" callback={ function(){} } />, div);
   });
 
-
-  it('should have a button trigger', () => {
-    const wrapper = mount(<Button buttonText="test" callback={ function(){} } />);
-    expect(wrapper.find('.Button').length).toEqual(0);
+  it('should have a default buttonText prop', () => {
+    expect(defaultWrapper.props().buttonText).to.not.equal(null);
+    expect(defaultWrapper.props().buttonText).to.not.equal(undefined);
+    expect(defaultWrapper.props().buttonText).to.equal('add buttonText prop');
+    expect(defaultWrapper.props().buttonText).to.not.equal('test button');
+    expect(defaultWrapper.props().buttonText).to.not.equal('');
   });
-
 
   it('should have a default buttonType prop', () => {
-    const wrapper = mount(<Button />);
-    expect(wrapper.props().buttonType).toEqual('button');
+    expect(defaultWrapper.props().buttonType).to.not.equal(null);
+    expect(defaultWrapper.props().buttonType).to.not.equal(undefined);
+    expect(defaultWrapper.props().buttonType).to.equal('button');
+    expect(defaultWrapper.props().buttonType).to.not.equal('submit');
+    expect(defaultWrapper.props().buttonType).to.not.equal('');
   });
 
-
-  it('should add buttonType prop to type attribute', () => {
-    const wrapper = mount(<Button buttonType="submit" />);
-    expect(wrapper.props().buttonType).toEqual(wrapper.props().buttonType);
+  it('should have a default theme prop', () => {
+    expect(defaultWrapper.props().theme).to.not.equal(null);
+    expect(defaultWrapper.props().theme).to.not.equal(undefined);
+    expect(defaultWrapper.props().theme).to.equal('');
+    expect(defaultWrapper.props().theme).to.not.equal('secondary');
   });
-
 
   it('should have a default callback prop', () => {
-    const wrapper = mount(<Button />);
-    expect(wrapper.props().callback).toEqual(wrapper.props().callback);
+    expect(defaultWrapper.props().callback).to.not.equal(null);
+    expect(defaultWrapper.props().callback).to.not.equal(undefined);
+    expect(defaultWrapper.props().callback).to.not.throw(Error);
+  });
+
+  it('should add buttonText to UI', () => {
+    expect(wrapper.find('.Button').text()).to.not.equal(null);
+    expect(wrapper.find('.Button').text()).to.not.equal(undefined);
+    expect(wrapper.find('.Button').text()).to.not.equal('');
+    expect(wrapper.find('.Button').text()).to.not.equal('add buttonText prop');
+    expect(wrapper.find('.Button').text()).to.equal('test button');
+  });
+
+  it('should add theme as Button class', () => {
+    expect(wrapper.find('.secondary')).to.not.equal(null);
+    expect(wrapper.find('.secondary')).to.not.equal(undefined);
+    expect(wrapper.find('.secondary').length).to.equal(1);
+  });
+
+  it('should add buttonType to Button type attribute', () => {
+    expect(wrapper.props().buttonType).to.not.equal(null);
+    expect(wrapper.props().buttonType).to.not.equal(undefined);
+    expect(wrapper.props().buttonType).to.not.equal('');
+    expect(wrapper.props().buttonType).to.not.equal('button');
+    expect(wrapper.props().buttonType).to.equal('submit');
   });
 
 });
