1dcfc59ae3ea665f840c1d37f9e37ed30f35f3bb
Casey Baggz <caseybaggz@gmail.com>
2016-12-06 14:36:06 -0600

    Add Icon tests

--- src/modules/Icon/index.js
+++ src/modules/Icon/index.js
@@ -3,21 +3,33 @@ import * as Icons from './Icons';
 import './Icon.css';
 
 const propTypes = {
-  name: PropTypes.string
+  color: PropTypes.string,
+  height: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  width: PropTypes.string
 };
 
 const defaultProps = {
-  name: ''
+  color: '#646471',
+  height: '26',
+  width: '26'
 };
 
 
-export default function Icon({ children, name, ...props }) {
+export default function Icon({...props}) {
+  const { children, name } = props;
   const IconComponent = Icons[name];
 
   return(
     <div className="Icon">
-      <IconComponent />
-      { children }
+      <IconComponent { ...props } />
+      <span className="Icon-children-wrapper">
+        { children }
+      </span>
     </div>
   );
 }
+
+
+Icon.propTypes = propTypes;
+Icon.defaultProps = defaultProps;
