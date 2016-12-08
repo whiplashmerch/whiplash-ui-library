9101c88898660158cfb5d7ced1f9ce30f7437513
Casey Baggz <caseybaggz@gmail.com>
2016-12-08 15:34:00 -0600

    Update Demo fonts

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,6 +1,7 @@
 import 'animate.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'fonts/index.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
