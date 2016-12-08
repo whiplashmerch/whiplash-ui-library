2ebc05237ece194a46cd34b539d041624b0d9b7b
Casey Baggz <caseybaggz@gmail.com>
2016-12-08 11:11:42 -0600

    StackedBoxes Component

--- demo/src/views/ItemView/index.js
+++ demo/src/views/ItemView/index.js
@@ -30,7 +30,7 @@ export default class ItemView extends Component {
     ];
 
     this.informationalIcons = [
-      'box', 'logistics'
+      'box', 'logistics', 'stacked_boxes'
     ];
 
     this.state = {
--- src/modules/InformationalIcon/Icons/index.js
+++ src/modules/InformationalIcon/Icons/index.js
@@ -1,2 +1,3 @@
 export { default as box } from './components/Box';
 export { default as logistics } from './components/Logistics';
+export { default as stacked_boxes } from './components/StackedBoxes';
