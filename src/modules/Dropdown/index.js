7571159147289b0df39167a642f4f157487cfc9a
Casey Baggz <caseybaggz@gmail.com>
2017-01-18 10:26:44 -0600

    Create <Dropdown />

---
+++ src/modules/Dropdown/index.js
@@ -0,0 +1,36 @@
+import React, { PropTypes } from 'react';
+import classnames from 'classnames';
+import './Dropdown.css';
+
+const propTypes = {
+  children: PropTypes.func,
+  left: PropTypes.bool,
+  open: PropTypes.bool,
+  right: PropTypes.bool
+};
+
+const defaultProps = {
+  left: false,
+  open: false,
+  right: false
+};
+
+
+export default function Dropdown({ children, left, open, right }) {
+  const MainClass = classnames('Dropdown', 'animated', 'fadeIn', { left, right });
+
+  if (!open) {
+    return null;
+  }
+
+  return(
+    <div className={ MainClass }>
+      <ul className="Dropdown-ul">
+        { children }
+      </ul>
+    </div>
+  );
+}
+
+Dropdown.propTypes = propTypes;
+Dropdown.defaultProps = defaultProps;
