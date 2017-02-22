1a80fec5be6f1941f4c746ea7b239cf2a86d758d
Casey Baggz <caseybaggz@gmail.com>
2017-02-22 09:52:50 -0600

    Update <AnimatedInput /> tests

--- src/modules/Input/components/AnimatedInput/Input.test.js
+++ src/modules/Input/components/AnimatedInput/Input.test.js
@@ -7,6 +7,8 @@ import AnimatedInput from './';
 
 describe('<AnimatedInput />', () => {
 
+  const testFn = () => console.log('');
+
   const defaultWrapper = mount(<AnimatedInput />);
 
   const wrapper = mount(
@@ -15,7 +17,7 @@ describe('<AnimatedInput />', () => {
       type="email"
       placeholder="test"
       maxLength="220"
-      onUserInput={(e) => console.log(e)}
+      onUserInput={ testFn }
       required
     />
   );
@@ -33,10 +35,38 @@ describe('<AnimatedInput />', () => {
     expect(defaultWrapper.props().inputLabel).to.not.equal('test label');
   });
 
+  it('should accept a inputLabel prop', () => {
+    expect(wrapper.props().inputLabel).to.not.equal(null);
+    expect(wrapper.props().inputLabel).to.not.equal(undefined);
+    expect(wrapper.props().inputLabel).to.not.equal('');
+    expect(wrapper.props().inputLabel).to.equal('test label');
+  });
+
   it('should have a default onUserInput prop', () => {
     expect(defaultWrapper.props().onUserInput).to.not.equal(null);
     expect(defaultWrapper.props().onUserInput).to.not.equal(undefined);
     expect(defaultWrapper.props().onUserInput).to.not.throw(Error);
+  });
+
+  it('should accept a onUserInput prop', () => {
+    expect(wrapper.props().onUserInput).to.not.equal(null);
+    expect(wrapper.props().onUserInput).to.not.equal(undefined);
+    expect(wrapper.props().onUserInput).to.not.throw(Error);
+    expect(wrapper.props().onUserInput).to.equal(testFn);
+  });
+
+  it('should allow all other props given', () => {
+    expect(wrapper.props().placeholder).to.not.equal(null);
+    expect(wrapper.props().placeholder).to.not.equal(undefined);
+    expect(wrapper.props().placeholder).to.not.equal('');
+    expect(wrapper.props().placeholder).to.equal('test');
+  });
+
+  it('should have a default active state', () => {
+    expect(wrapper.state().active).to.not.equal(null);
+    expect(wrapper.state().active).to.not.equal(undefined);
+    expect(wrapper.state().active).to.not.equal('false');
+    expect(wrapper.state().active).to.equal(false);
   });
 
   it('should display the inputLabel in the UI', () => {
