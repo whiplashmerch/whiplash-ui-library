e58444c0564d9f1dc56a89478e76744ec9e95db6
Casey Baggz <caseybaggz@gmail.com>
2017-02-28 09:22:46 -0600

    Update Icon children tests

--- tests/ArrowRight.test.js
+++ tests/ArrowRight.test.js
@@ -16,13 +16,6 @@ describe('<arrow_right />', () => {
     ReactDOM.render(<arrow_right />, div);
   });
 
-  it('should have a default iconColor prop', () => {
-    expect(defaultWrapper.props().iconColor).to.not.equal(null);
-    expect(defaultWrapper.props().iconColor).to.not.equal(undefined);
-    expect(defaultWrapper.props().iconColor).to.not.equal('');
-    expect(defaultWrapper.props().iconColor).to.equal('#646471');
-  });
-
   it('should take a iconColor prop', () => {
     expect(wrapper.props().iconColor).to.not.equal(null);
     expect(wrapper.props().iconColor).to.not.equal(undefined);
