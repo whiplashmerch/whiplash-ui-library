26153c269d6cdd701529da766f9fbf5b41eab60d
Casey Baggz <caseybaggz@gmail.com>
2017-02-28 07:23:36 -0600

    Create initial styles for <DatePicker />

--- src/modules/DatePicker/index.js
+++ src/modules/DatePicker/index.js
@@ -3,10 +3,34 @@ import './DatePicker.css';
 
 
 export default class DatePicker extends Component {
+
+  // PRIVATE
+
+  _toggleMap = () => {
+    console.log('toggle map');
+  }
+
   render() {
+    const { ...props } = this.props;
+
     return(
       <div className="DatePicker">
-        date picker
+        <div className="DatePicker-input-wrapper">
+          <input
+            { ...props }
+            className="DatePicker-input"
+            onClick={ this._toggleMap }
+            placeholder="select date"
+            ref={ ref => this.inputRef = ref }
+            type="text"
+            value=""
+            readOnly
+          />
+        </div>
+
+        <div className="DatePicker-map-wrapper">
+          
+        </div>
       </div>
     );
   }
