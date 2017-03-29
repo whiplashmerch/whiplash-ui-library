fa8e53316e4c6b4d613b6b012e2e729682590b2d
Casey Baggz <caseybaggz@gmail.com>
2017-03-29 09:28:43 -0500

    Update ValidComponentChildren tests

--- tests/ValidateComponentChildren.test.js
+++ tests/ValidateComponentChildren.test.js
@@ -23,63 +23,79 @@ describe('ValidComponentChildren util', () => {
   });
 
   it('should have a healthy forEach func', () => {
-    const child = <div>test</div>;
+    const child = [<div />];
+    const testFn = sinon.spy();
     const orig = ValidComponentChildren.forEach;
     const func = sinon.spy(ValidComponentChildren.forEach);
 
     expect(orig).to.not.equal(null);
     expect(orig).to.not.equal(undefined);
     expect(func.threw).to.not.equal(true);
+    expect(orig(child, testFn)).to.equal(undefined);
+    expect(testFn.called).to.equal(true);
   });
 
   it('should have a healthy count func', () => {
-    const child = <div>test</div>;
+    const childs = [<div />, <div />];
     const orig = ValidComponentChildren.count;
     const func = sinon.spy(ValidComponentChildren.count);
 
     expect(orig).to.not.equal(null);
     expect(orig).to.not.equal(undefined);
     expect(func.threw).to.not.equal(true);
+    expect(orig(childs)).to.equal(2);
   });
 
   it('should have a healthy filter func', () => {
-    const child = <div>test</div>;
+    const children = [<div />, <div />];
+    const testFn   = sinon.spy();
     const orig = ValidComponentChildren.filter;
     const func = sinon.spy(ValidComponentChildren.filter);
 
     expect(orig).to.not.equal(null);
     expect(orig).to.not.equal(undefined);
     expect(func.threw).to.not.equal(true);
+    expect(orig(children, testFn).length).to.equal(0);
+    expect(testFn.called).to.equal(true);
   });
 
   it('should have a healthy find func', () => {
-    const child = <div>test</div>;
+    const children = [<div />, <span />, <div />];
+    const testFn = sinon.spy();
     const orig = ValidComponentChildren.find;
     const func = sinon.spy(ValidComponentChildren.find);
 
     expect(orig).to.not.equal(null);
     expect(orig).to.not.equal(undefined);
     expect(func.threw).to.not.equal(true);
+    expect(orig(children, testFn, {})).to.equal(undefined);
+    expect(testFn.called).to.equal(true);
   });
 
   it('should have a healthy every func', () => {
-    const child = <div>test</div>;
+    const children = [<div />, <span />, <div />];
+    const testFn = sinon.spy();
     const orig = ValidComponentChildren.every;
     const func = sinon.spy(ValidComponentChildren.every);
 
     expect(orig).to.not.equal(null);
     expect(orig).to.not.equal(undefined);
     expect(func.threw).to.not.equal(true);
+    expect(orig(children, testFn, {})).to.equal(false);
+    expect(testFn.called).to.equal(true);
   });
 
   it('should have a healthy some func', () => {
-    const child = <div>test</div>;
+    const children = [<div />, <span />, <div />];
+    const testFn = sinon.spy();
     const orig = ValidComponentChildren.some;
     const func = sinon.spy(ValidComponentChildren.some);
 
     expect(orig).to.not.equal(null);
     expect(orig).to.not.equal(undefined);
     expect(func.threw).to.not.equal(true);
+    expect(orig(children, testFn, {})).to.equal(false);
+    expect(testFn.called).to.equal(true);
   });
 
   it('should have a healthy toArray func', () => {
@@ -90,6 +106,8 @@ describe('ValidComponentChildren util', () => {
     expect(orig).to.not.equal(null);
     expect(orig).to.not.equal(undefined);
     expect(func.threw).to.not.equal(true);
+    expect(orig(child).length).to.not.equal(0);
+    expect(orig(child).length).to.equal(1);
   });
 
 });
