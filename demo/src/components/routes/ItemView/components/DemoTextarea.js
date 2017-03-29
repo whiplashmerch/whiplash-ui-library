6e15a40c836787b48d988d36d541c15f9845f46d
Casey Baggz <caseybaggz@gmail.com>
2017-03-29 08:19:05 -0500

    Create <DemoTextarea />

---
+++ demo/src/components/routes/ItemView/components/DemoTextarea.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Textarea } from 'src';
+
+
+export default function DemoBreadcrumb() {
+  return(
+    <Textarea
+      inputLabel="Add a Note"
+      placeholder="Dear Harry, ..."
+    />
+  );
+}
