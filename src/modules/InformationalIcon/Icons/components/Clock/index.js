b05ac2acb704901b62920c936edb31fc0dff7d2a
Casey Baggz <caseybaggz@gmail.com>
2017-04-05 15:31:27 -0500

    Update <InformationalIcon /> static refs for performance

--- src/modules/InformationalIcon/Icons/components/Clock/index.js
+++ src/modules/InformationalIcon/Icons/components/Clock/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const clock = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g fill="none" fillRule="evenodd" stroke="#0ED9AC" transform="translate(1 1)"><circle cx="11" cy="11" r="11"/><path d="M11 11V4m0 15v-1m0-7h5M3 11h1" strokeLinecap="round"/></g></svg>;
+const clockIcon = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g fill="none" fillRule="evenodd" stroke="#0ED9AC" transform="translate(1 1)"><circle cx="11" cy="11" r="11"/><path d="M11 11V4m0 15v-1m0-7h5M3 11h1" strokeLinecap="round"/></g></svg>;
 
 export default function Clock() {
-  return clock;
+  return clockIcon;
 }
