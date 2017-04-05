6e1eeef7df7fe9fe53e69f437ac80699a6ad6ba3
Casey Baggz <caseybaggz@gmail.com>
2017-04-05 15:23:56 -0500

    Update clock

--- src/modules/InformationalIcon/Icons/components/Clock/index.js
+++ src/modules/InformationalIcon/Icons/components/Clock/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-export default function Clock({ iconColor }) {
-  return(
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>clock</title><g fill="none" fillRule="evenodd" stroke="#0ED9AC" transform="translate(1 1)"><circle cx="11" cy="11" r="11"/><path d="M11 11V4m0 15v-1m0-7h5M3 11h1" strokeLinecap="round"/></g></svg>
-  );
+const clock = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g fill="none" fillRule="evenodd" stroke="#0ED9AC" transform="translate(1 1)"><circle cx="11" cy="11" r="11"/><path d="M11 11V4m0 15v-1m0-7h5M3 11h1" strokeLinecap="round"/></g></svg>;
+
+export default function Clock() {
+  return clock;
 }
